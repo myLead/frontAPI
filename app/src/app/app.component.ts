30520f32916b9b9f813734d2afb67e1e84927de8
Matheus Souza <matheus007junior@hotmail.com>
2017-12-10 21:05:27 -0300

    Metodo get no console e tentativa do post(incompleto)

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+
 import { Observable } from 'rxjs/Observable';
 import { Usuario } from './Usuario';
 import { UsuarioService } from './app.service';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
@@ -9,15 +12,40 @@ import { UsuarioService } from './app.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  
 	usuario: Usuario;
-  title = 'app';
+  title = 'MyLead';
+  data: any = null;
+  data2: {"email": "sydney@fife",
+  "password": "pistol"};
+  private API_URL = "https://reqres.in/api"
 
-  constructor(private _usuarioService: UsuarioService) {
-  	this._usuarioService.getUsuario()
-            .subscribe((data: Usuario) => this.usuario = data,
-            error => console.log(error));
-
+  constructor(private _http: Http) {
+    this.getMyBlog();
+    this.createAccount(this.data2);
+    }
+  private getMyBlog() {
+      return this._http.get(this.API_URL + '/users?page=2')
+                  .map((res: Response) => res.json())
+                   .subscribe(data => {
+                          this.data = data.data;
+                          console.log(this.data);
+                  });
     }
 
+
+  private  createAccount(data2) {
+      return new Promise((resolve, reject) => {
+        this._http.post('https://reqres.in/api/login', data2)
+          .subscribe((result: any) => {
+            resolve(result.json())
+          },
+          (error) => {
+            reject(error.json())
+          });
+        
+      });
+  }
 
 }
