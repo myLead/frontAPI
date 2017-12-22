21831eeb0cbeb185602bab82a3b3d28ebaf29941
LeandroBraz <Leandrocassio10braz@gmail.com>
2017-12-21 23:13:48 -0300

    Login fucionando

--- app/src/app/modals/modal-login/modal-login.component.ts
+++ app/src/app/modals/modal-login/modal-login.component.ts
@@ -1,33 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../user.service';
 import { Router, RouterModule} from '@angular/router';
+import { Http } from '@angular/http';
+
 @Component({
   selector: 'app-modal-login',
   templateUrl: './modal-login.component.html',
   styleUrls: ['./modal-login.component.css']
 })
 export class ModalLoginComponent implements OnInit {
+  private API_URL = "https://mylead-api.herokuapp.com";
+  private formulario = {"email_usuario": null, "senha_usuario": null}
 
-  constructor(private user: UserService, private router:Router) { }
+
+  constructor(private user: UserService, private router:Router, private _http: Http) { }
 
   ngOnInit() {
   }
  
-  longinUser() {
-    alert('oiwe');
-  /*  e.preventDefault;
-    console.log(e);
-    var email = e.target.elements[0].value;
-    var senha = e.target.elements[1].value;
+  
+  private onSubmitLogin(form){
     
-    
-    if (email == 'admin@hotmail.com' && senha == 'admin'){
-      this.user.setUserloggedIn();
-      this.router.navigate(['/dashboard'])
+        form._directives.forEach(element => {
+          switch (element.name) {
+            case 'loginEmail':
+              this.formulario.email_usuario = element.viewModel;
+              break;
+            case 'loginSenha':
+              this.formulario.senha_usuario = element.viewModel;
+              break;
+            default:
+              break;
+          }
+        });
+
+        this.LoginAccount(this.formulario);
+        
+      }
+
+      private LoginAccount(usuario) {
+        return new Promise((resolve, reject) => {
+          this._http.post(this.API_URL + '/login', usuario)
+            .subscribe((result: any) => {
+              if (result.json()) {
+                if (result.json().status == "success"){
+                  alert(result.json().message)
+                }else{
+                  alert(result.json().message)
+                }
+              }
+            },
+            (error) => {
+              reject(error.json())
+            });
+        });
     }
-    
-   /* console.log(email, senha);
-    return false;*/
-  }
+ 
 
 }
