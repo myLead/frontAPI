f0ad4d005f27ff8ee5ec6446a5333c4462dff101
LeandroBraz <Leandrocassio10braz@gmail.com>
2017-12-22 10:01:57 -0300

    CanActivate sem comentario

--- app/src/app/app.routing.ts
+++ app/src/app/app.routing.ts
@@ -12,7 +12,7 @@ const APP_ROUTS: Routes = [
     },
     {
         path: 'dashboard',
-        //canActivate: [AuthguardGuard],
+        canActivate: [AuthguardGuard],
         component: DashboardComponent
     }
 ]
